3a53028e599b1d99d062faf5dea8a2adf272b8b7
Ivan Fenenko <ivanfenenko@gmail.com>
2017-07-04 09:45:48 +0200

    instrumentation test sample with mock server

--- app/src/androidTest/java/me/ivanfenenko/tablebooking/UserListInstrumentationTest.java
+++ app/src/androidTest/java/me/ivanfenenko/tablebooking/UserListInstrumentationTest.java
@@ -13,6 +13,8 @@ import org.junit.runner.RunWith;
 import java.io.IOException;
 
 import me.ivanfenenko.tablebooking.customers.UserListActivity;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -22,26 +24,30 @@ import me.ivanfenenko.tablebooking.customers.UserListActivity;
 @RunWith(AndroidJUnit4.class)
 public class UserListInstrumentationTest {
 
+    private MockWebServer mockWebServer = new MockWebServer();
+
     @Rule
     public ActivityTestRule<UserListActivity> activityTestRule =
             new ActivityTestRule<>(UserListActivity.class, false, true);
 
     @Before
     public void setup() throws IOException {
+        mockWebServer.start();
     }
 
     @After
     public void tearDown() throws IOException {
+        mockWebServer.shutdown();
     }
 
     @Test
     public void show_list() throws IOException {
         UserListActivity activity = activityTestRule.getActivity();
 
+        mockWebServer.enqueue(new MockResponse());
+
         activityTestRule.launchActivity(new Intent());
 
-//        onView(witrect(R.id.recycler_view)
-//                .atPositionOnView(1, R.id.ofElementYouWantToCheck))
-//                .check(matches(withText("Test text")));
+            
     }
 }
---
+++ app/src/androidTest/java/me/ivanfenenko/tablebooking/utils/RecyclerViewMatcher.java
@@ -0,0 +1,8 @@
+package me.ivanfenenko.tablebooking.utils;
+
+/**
+ * Created by klickrent on 04.07.17.
+ */
+
+public class RecyclerViewMatcher {
+}
